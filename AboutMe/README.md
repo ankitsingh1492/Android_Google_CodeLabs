3d008ffc4f936bd781d01496278ba8328e686c8a
Ankit <ankit.singh@clarivate.com>
2020-03-30 13:45:33 +0530

    added data bindings notes and code

--- AboutMe/README.md
+++ AboutMe/README.md
@@ -12,4 +12,34 @@ This project is the part of the **Android Kotlin Fundamentals 02.1**.
 * Adding more complex UI by combining ScrollView , Image View, EditText , buttons and Layout Views.
 * Added user interaction through edit text.
 * Finding views using findViewById() method.
+* Added click listeners to the nickname text.
+
+## Learning Update 3
+* Removed findViewById() added data bindings.
+
+### data binding (Udacity Notes - Credits Udacity)
+* The idea behind data binding is to create an object that connects two pieces of distant information
+together at compile time , so that you don't have to look for it at the runtime.
+* The object that surfaces these bindings to you is called the Binding object. It is created by the
+compiler.
+
+### Data binding vs findViewById
+* findViewById is a costly operation because it traverses the view hierarchy every time it is called.
+* with data binding enabled, the compiler creates references to all views in a <layout> that have an
+id, and gathers them in a binding object.
+![Binding](/Readme Images/Binding1.png)
+
+Image Credits - udacity
+
+### Data binding views and data
+* Updating data and then updating the data displayed in views is cumbersome and a source of errors.
+Keeping the data in the view also violates the separation of data and presentation.
+* Data binding solves both of these problems. You keep data in a data class. You add a <Data> block to the
+<layout> to identify the data as variables to use with the views. Views reference the variables.
+* The compiler generates a binding object that binds the views and data,
+* In code, you reference and update the data through the binding object, which updates the data, and thus
+what is displayed in the view.
+![Binding](/Readme Images/Binding2.png)
+
+Image Credits - udacity
 
--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aboutme
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,32 +9,62 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName( "Ankit signh")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    binding.myName = myName
+        //now we can find the button using the binding.doneButton
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+//        }
+
+        binding.doneButton.setOnClickListener{
             addNickname(it)
+        }
+
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
+        binding.nicknameText.text = binding.nicknameEdit.text
 
         //handling the transitions
-        editText.visibility = View.GONE
+        binding.nicknameEdit.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.nicknameText.visibility = View.VISIBLE
 
         //hide the keyboard
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        //handling transitions
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
